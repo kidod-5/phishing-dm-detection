ef5cfac91454e73afcc0ac2744a7f3c14bd929ef
golavarri <golavarri@yahoo.com>
2024-11-29 17:41:41 -0500

    Troubleshooting frontend/backend integration

--- backend-python/main.py
+++ backend-python/main.py
@@ -16,7 +16,7 @@ origins = [
 # server config
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://isthisphishing.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
